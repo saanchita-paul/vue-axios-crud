84aab9066a54eb97f4cda2476e03195b3a1f51f2
Sanchita Paul <68769067+saanchita-paul@users.noreply.github.com>
2022-11-29 23:06:38 +0600

    crud using axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <button @click="getUsers">Get users</button> <br />
+  </div>
+    <div class="users" v-for="user in users" :key="user.id">
+      <h2>{{ user.title }}</h2>
+      <!-- <p>{{ user.email }}</p> -->
+    </div>
+  <div class="user-add">
+    <h2>Add user</h2>
+    <input type="text" v-model="title" /> <br />
+    <!-- <input type="text" v-model="email" /> <br /> -->
+    <button @click="storeUser">Store</button>
+  </div>
+  <div class="user-update">
+    <h2>Update user</h2>
+    <input type="text" v-model="user.title" /> <br />
+    <!-- <input type="text" v-model="user.email" /> <br /> -->
+    <button @click="updateUser">Update</button>
+  </div>
+  <div class="users" v-for="user in users" :key="user.id">
+    <h2>{{ user.title }}</h2>
+    <!-- <p>{{ user.email }}</p> -->
+
+    <button @click="editUser(user)">Edit</button>
+    <button @click="deleteUser(user.id)">Delete</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  name: "App",
+  data() {
+    return {
+      users: [],
+      user: {},
+      // email: '',
+      title: '',
+    };
+  },
+  methods: {
+    async getUsers() {
+      try {
+        const users = await axios.get(
+          "http://127.0.0.1:8000/api/list"
+        );
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+        this.users = users.data;
+        console.log(this.users);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async storeUser() {
+      try {
+        const user = await axios.post(
+          "http://127.0.0.1:8000/api/create", 
+          {
+            title: this.title,
+            // email : this.email
+          }
+        );
+
+        console.log(user)
+      } catch(e) {
+        console.log(e);
+      }
+    },
+    async updateUser() {
+      try {
+        const user = await axios.put(
+          "http://127.0.0.1:8000/api/update/" + this.user.id,
+          {
+            title: this.user.title,
+            // email: this.user.email,
+          }
+        );
+
+        console.log(user.data);
+        alert("User updated!");
+      } catch (e) {
+        console.log(e);
+      }
+    },
+    async editUser(user) {
+      this.user.title = user.title;
+      // this.user.email = user.email;
+      this.user.id = user.id;
+    },
+    async deleteUser(id) {
+      let x = window.confirm("You want to delete the user?");
+
+      if (x) {
+        const user = await axios.delete(
+          "http://127.0.0.1:8000/api/destroy/" + id
+        );
+
+        console.log(user);
+        alert("User deleted!");
+      }
+    },
+  },
+};
+</script>
